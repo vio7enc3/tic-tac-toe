2b21c6702ccd42f4371c9eba538407ae54a62475
vio7enc3 <nicksakerin96@gmail.com>
2021-03-02 13:40:13 +0500

    feat: create footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Footer.scss'
 
+import logo from '../../assets/rs_school_js.svg'
+
 export default function Footer() {
   return (
     <footer className="footer">
-      
+      <a className="footer__logo" href="https://rs.school/js/">
+        <img src={logo} alt="RSSchool" />
+      </a>
+      <a href="https://github.com/vio7enc3" className="footer__name">
+        Nikita Sakerin
+      </a>
     </footer>
   )
 }
