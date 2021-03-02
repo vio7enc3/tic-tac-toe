05ad6cff5294a4fedb719dd821476a0d76f8084c
vio7enc3 <nicksakerin96@gmail.com>
2021-03-02 13:40:40 +0500

    feat: integrate burger and sidemenu in header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './Header.scss'
 
 import logo from '../../assets/icons/close.svg'
+import { Burger } from '../Burger'
 
 export default function Header() {
   return (
     <header className="header">
+      <div className="header__burger">
+        <Burger />
+      </div>
       <div className="header__logo">
         <img src={logo} alt=""/>
       </div>
