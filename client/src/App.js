40e70e5c2bf23c1d857a03e5d484bbbd757f5890
vio7enc3 <nicksakerin96@gmail.com>
2021-03-03 10:50:59 +0500

    feat: add fullscreen button

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import { Deskboard } from "./components/Deskboard";
 import { MainLayout } from "./components/MainLayout";
 import { GameContext } from "./context/GameContext";
+import {
+  FullScreen,
+  useFullScreenHandle,
+} from "react-full-screen";
 
 function App() {
+  const handle = useFullScreenHandle()
+  const fsIsActive = handle.active
+
   return (
     <>
       <GameContext>
-        <MainLayout>
-          <Deskboard />
-        </MainLayout>
+        <FullScreen handle={handle}>
+          <MainLayout>
+            <div className="app__fullscreen" onClick={fsIsActive ? handle.exit : handle.enter}>{fsIsActive ? 'Exit Fullscreen' : 'Enter Fullscreen'}</div>
+            <Deskboard />
+          </MainLayout>
+        </FullScreen>
       </GameContext>
     </>
   );
