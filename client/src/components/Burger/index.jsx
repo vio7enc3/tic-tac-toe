18180b3f6af6b931b18822798252a41d471381af
vio7enc3 <nicksakerin96@gmail.com>
2021-03-03 10:26:06 +0500

    feat: add stats block and history of steps

--- client/src/components/Burger/index.jsx
+++ client/src/components/Burger/index.jsx
@@ -1,34 +1,71 @@
 import React from 'react'
+import { Context } from '../../context/GameContext'
 import './Burger.scss'
 
 export const Burger = () => {
-  const [open, setOpen] = React.useState(false)
+  const [show, setShow] = React.useState(false)
+  const { history, setHistory, stepCount, xWins, oWins, setDeskboard, setXIsNext } = React.useContext(Context)
 
-  const openHandler = () => {
-    setOpen(!open)
+  const showHandler = () => {
+    setShow(!show)
   }
+
+  const changeMoveHandler = (e) => {
+    let arr = Array(9).fill(null)
+    for (let i = 0; i < history[+e.target.id].length; i ++) {
+      arr[i] = history[+e.target.id][i]
+    }
+    console.log(arr);
+    setDeskboard(arr)
+    history.splice(+e.target.id, history.length - 1)
+    if (+e.target.id % 2 === 0) {
+      setXIsNext(false)
+    } else setXIsNext(true)
+    console.log(e.target.id);
+  }
+
+  console.log(history);
+
   return (
     <>
-      <div onClick={openHandler} role="button" className={`burger ${open && 'active'}`}>
+      <div onClick={showHandler} role="button" className={`burger ${show && 'active'}`}>
         <span></span>
       </div>
 
-      <div className={`sidemenu ${open && 'active'}`}>
+      <div className={`sidemenu ${show && 'active'}`}>
         <div className="sidemenu__stats">
-          <span>Number of steps: 0</span>
+          <span>
+            Number of steps:
+            {' '}
+            {stepCount}
+          </span>
           <div className="sidemenu__stats-count">
             <div className="sidemenu__stats-count--left">
-              X's Wins: 0
-          </div>
+              X's Wins:
+              {' '}
+              {xWins}
+            </div>
             <div className="sidemenu__stats-count--right">
-              O's Wins: 0
-          </div>
+              O's Wins:
+              {' '}
+              {oWins}
+            </div>
           </div>
         </div>
         <div className="sidemenu__history">
-
+          <h3 className="sidemenu__history--title">
+            History
+          </h3>
+          {history.length > 0 && history.map((item, i) => (
+            <div key={i} className="sidemenu__history-col">
+              <button className="sidemenu__history-col--item" id={i} onClick={changeMoveHandler}>
+                Move to step
+                  {' '}
+                {i + 1}
+              </button>
+            </div>
+          ))}
         </div>
-
       </div>
     </>
   )
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -3,8 +3,10 @@ import './Header.scss'
 
 import logo from '../../assets/icons/close.svg'
 import { Burger } from '../Burger'
+import { Context } from '../../context/GameContext'
 
 export default function Header() {
+  const { xWins, oWins, stepCount } = React.useContext(Context)
   return (
     <header className="header">
       <div className="header__burger">
@@ -12,6 +14,18 @@ export default function Header() {
       </div>
       <div className="header__logo">
         <img src={logo} alt=""/>
+      </div>
+      <div className="header__stats">
+        <div className="header__stats-count">
+          Step:
+          {' '}
+          {stepCount}
+        </div>
+        <div className="header__stats-total">
+          X : O
+          {'  -  '}
+          <span>{`${xWins} : ${oWins}`}</span>
+        </div>
       </div>
     </header>
   )
